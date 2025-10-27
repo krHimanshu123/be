7b299b557b511f16b7b520e4cc6c007fe7a96129
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-27 21:44:25 +0530

    Fix error handling in product upload method

--- src/main/java/com/klu/ecommerce/controller/ProductController.java
+++ src/main/java/com/klu/ecommerce/controller/ProductController.java
@@ -58,6 +58,7 @@ public class ProductController {
             return ResponseEntity.ok("Product uploaded successfully!");
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload product image");
+            
         }
     }
 
