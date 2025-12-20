0aea8106317a20ff2a666666dbc59a62168faab9
Himanshu Kumar <162711386+krHimanshu123@users.noreply.github.com>
2025-12-20 23:49:01 +0530

    Add DaoAuthenticationProvider to authentication config

--- src/main/java/com/klu/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/klu/ecommerce/security/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
+        
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
